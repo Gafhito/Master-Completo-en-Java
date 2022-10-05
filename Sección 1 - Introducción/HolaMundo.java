555179b6a881488db92b396dfb15147a685ea604
Gafhito <gonzalo.ferrari@outlook.es>
2022-10-05 19:57:14 -0300

    Sección 1: Introducción

--- Sección 1 - Introducción/HolaMundo.java
+++ Sección 1 - Introducción/HolaMundo.java
@@ -1,3 +1,10 @@
+/*
+Máster Completo en Java de Cero a Experto
+
+Sección 1: Introducción
+Video 7: Ejemplo Hola Mundo en Java
+ */
+
 public class HolaMundo {
 
     public static void main(String[] args) {
--- Sección 1 - Introducción/MiNombre.java
+++ Sección 1 - Introducción/MiNombre.java
@@ -1,3 +1,10 @@
+/*
+Máster Completo en Java de Cero a Experto
+
+Sección 1: Introducción
+Tarea 1: Imprime tu nombre en consola
+ */
+
 public class MiNombre {
 
     public static void main(String[] args) {
